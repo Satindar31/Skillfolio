334b3d824980637ef18b7ee285b0781a2cb62f05
Satindar <ibrahimasif711@outlook.com>
2023-07-29 13:30:09 +0530

    Form UI
    Completed the Form UI + Added metadata

--- components/resumes/form.tsx
+++ components/resumes/form.tsx
@@ -17,34 +17,102 @@ import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 
 export default function ResumeForm() {
-  const resumeSchema = z.object({
-    name: z.string(),
-    aboutMeLine1: z.string(),
-    aboutMeLine2: z.string(),
-    aboutMeLine3: z.string(),
-    aboutMeLine4: z.string(),
-    aboutMeLine5: z.string(),
-    educationLine1: z.string(),
-    educationLine2: z.string(),
-    educationLine3: z.string(),
-    educationLine4: z.string(),
-    educationLine5: z.string(),
-    previsousExperianceLine1: z.string(),
-    previsousExperianceLine2: z.string(),
-    previsousExperianceLine3: z.string(),
-  });
-
-``
+  ``;
   function onSubmit(): any {
     console.log("submitted");
   }
   return (
     <form className="bg-transparent" onSubmit={onsubmit?.call}>
-      <Label>Name</Label>
-      <Input placeholder="Jhon Doe" />
-      <span className="m-2 sm:m-1" />
+      <Label className="text-center">Name</Label>
+      <Input
+        type="text"
+        className="left-1/2 max-w-md bg-transparent border-slate-800"
+        placeholder="Jhon Doe"
+      />
+      <span className="m-4 sm:m-2" />
+      <div className="grid grid-cols-3 sm:grid-col-2">
+        <div className="m-2 p-2">
+          <Label>About you line 1</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="Hi my name is ..."
+          />
 
-      <Label></Label>
+          <Label>About you line 2</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I was born in ... on ..., ..."
+          />
+
+          <Label>About you line 3</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I am based in ..."
+          />
+
+          <Label>About you line 4</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I have a passion for ..."
+          />
+
+          <Label>About you line 5</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="My hobbies are ..."
+          />
+        </div>
+        <div className="m-2 p-2">
+          <Label>Education line 1</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I started my education in ..."
+          />
+
+          <Label>Education line 2</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I started high school in ..."
+          />
+
+          <Label>Education line 3</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I graduated high school in ..."
+          />
+
+          <Label>Education line 4</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I went to ... college/university for ..."
+          />
+
+          <Label>Education line 5</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I completed university/college in ..."
+          />
+        </div>
+        <div className="m-2 p-2">
+          <Label>Experience line 1</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I started my experience in ..."
+          />
+
+          <Label>Experience line 2</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I started high school in ..."
+          />
+
+          <Label>Experience line 3</Label>
+          <Input
+            className="bg-transparent border-slate-800 active:border active:border-slate-800"
+            placeholder="I graduated high school in ..."
+          />
+        </div>
+      </div>
     </form>
   );
 }
